053f8d91bc42c58c7933c1278696162a5c440f78
niels <nielsraddatz@gmx.de>
2025-05-25 20:51:07 +0200

    remove vintf manifest fragments introduced by ROM

--- recovery/root/system/bin/fix-invalid-vintf-manifest.sh
+++ recovery/root/system/bin/fix-invalid-vintf-manifest.sh
@@ -1,5 +1,9 @@
 #!/system/bin/sh
 
-#if /system/bin/grep -Fq "androidboot.mode=recovery" /proc/cmdline; then
-    rm /system/etc/vintf/manifest/boot-service*.xml
-#fi
+if /system/bin/grep -qe "androidboot.mode=recovery" -e "twrpfastboot" /proc/cmdline; then
+    for f in /system/etc/vintf/manifest/* /vendor/etc/vintf/manifest/*; do
+        if /system/bin/grep -qFvz "${f:1}" /ramdisk-files.txt; then
+            rm $f
+        fi
+    done
+fi
